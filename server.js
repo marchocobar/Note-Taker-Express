190e401d6708420d281b9b0feda9c4109f6d9024
Marissa Baron <mariluvv23@gmail.com>
2022-11-15 20:10:08 -0500

    added get and post routes

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
+const notesData = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
 
 
 app.use(express.static('public'));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+//GET api route for db.json
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+app.post('/api/notes', (req, res) => {
+    // Inform the client that their POST request was received
+    res.json(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received to add a note`);
+  });
+
+  app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
